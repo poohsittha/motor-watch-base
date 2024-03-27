2f811ad084a5d99c03e9b6feb2c64d312c3f88cd
poohsittha <163396081+poohsittha@users.noreply.github.com>
2024-03-27 20:11:24 +0700

    _Motor_watch_base.ino

--- _Motor_watch_base.ino
+++ _Motor_watch_base.ino
@@ -1,30 +1,25 @@
 const int speedPin = 9;
 const int directionPin1 = 8;
 const int directionPin2 = 7;
-const int buttonPin = 6; // Assuming the button is connected to pin 2
+const int buttonPin = 6; 
 bool motorRunning = false;
 
 void setup() {
   pinMode(speedPin, OUTPUT);
   pinMode(directionPin1, OUTPUT);
   pinMode(directionPin2, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP); // Enable internal pull-up resistor for the button
+  pinMode(buttonPin, INPUT_PULLUP); 
 }
 
 void loop() {
-  // Check if the button is pressed
-  if (digitalRead(buttonPin) == LOW) {
-    // If the button is pressed, toggle the motor state
+  if (digitalRead(buttonPin) == LOW) {    
     motorRunning = !motorRunning;
-    // Delay to debounce the button press
     delay(1000);
   }
 
-  // If the motor is running, control its speed
   if (motorRunning) {
     speedControl();
   } else {
-    // If the motor is not running, turn it off
     digitalWrite(directionPin1, LOW);
     digitalWrite(directionPin2, LOW);
   }
